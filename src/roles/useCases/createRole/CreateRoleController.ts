812e81fe09e22deaa8948f6fe525670b1ef8c533
Tiago de Jesus Sousa Gama <tiago.gama@Gipsyys-MacBook-Pro.local>
2023-09-08 13:58:46 -0300

    createUseCase createRole

--- src/roles/useCases/createRole/CreateRoleController.ts
+++ src/roles/useCases/createRole/CreateRoleController.ts
@@ -3,9 +3,10 @@ import { CreateRoleUseCase } from './CreateRoleUseCase'
 
 export class CreateRoleController {
   constructor(private createRoleUseCase: CreateRoleUseCase) {}
+
   handle(request: Request, response: Response): Response {
     const { name } = request.body
-    const role = this.createRoleUseCase.execute(name)
+    const role = this.createRoleUseCase.execute({ name })
 
     return response.status(201).json(role)
   }
