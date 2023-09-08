61c3d9e4570321ca1a9c5232b04f5d6b4d034d10
Tiago de Jesus Sousa Gama <tiago.gama@Gipsyys-MacBook-Pro.local>
2023-09-08 12:24:05 -0300

    feat: add role entity

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express'
-import { v4 as uuidv4 } from 'uuid'
-
+import { Role } from './entities/role'
 const rolesRouter = Router()
 
-const roles = []
+const roles: Role[] = []
 
 rolesRouter.post('/', (request, response) => {
   const { name } = request.body
 
-  const role = {
-    id: uuidv4(),
+  const role = new Role()
+
+  Object.assign(role, {
     name,
     created_att: new Date(),
-  }
+  })
 
   roles.push(role)
 
