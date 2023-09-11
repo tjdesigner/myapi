06fe41a03758057d7e58737dd1389e193089e4c3
Tiago de Jesus Sousa Gama <tiago.gama@Gipsyys-MacBook-Pro.local>
2023-09-11 11:24:02 -0300

    feat: config database connection

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,35 +1,16 @@
 import 'dotenv/config'
-import express, { Request, Response, NextFunction } from 'express'
-import cors from 'cors'
-import 'express-async-errors'
-import swaggerUi from 'swagger-ui-express'
+import 'reflect-metadata'
 
-import { routes } from './routes'
-import { AppError } from '../erros/AppError'
-import swaggerFile from '../../swagger.json'
+import { app } from './app'
+import { dataSource } from '@shared/typeorm'
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
-app.use(routes)
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response
-        .status(error.statusCode)
-        .json({ status: 'error', message: error.message })
-    }
-    console.log(error)
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}!🚀`)
     })
-  },
-)
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server started on port ${process.env.PORT}!🚀`)
-})
+  })
+  .catch(err => {
+    console.error('Error during Data Source initialization', err)
+  })
